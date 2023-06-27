08456019e3826503882150422bb421057df8f7e7
Cesar Nasr <cesar.nasr@360mea.com>
2023-06-27 13:14:35 +0300

    WIP : - Added dark theme support

--- app/src/main/java/com/example/githubtrends/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubtrends/presentation/ui/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.githubtrends.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
-import com.example.githubtrends.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.githubtrends.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        val view = binding.root
+        setContentView(view)
+        switchDarkTheme()
+    }
+
+    private fun switchDarkTheme() {
+
+        binding.toolbarLayout.nightModeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+                }
+            }
+
+        }
     }
 }
